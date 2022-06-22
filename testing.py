a43cea458469d3cbb6c92f5058f8f0f18da6eedd
sahanabistappa <51372995+sahanabistappa@users.noreply.github.com>
2022-06-22 17:11:07 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -6,6 +6,6 @@ if __name__=="__main__":
   cmd_arg=sys.argv[1]
   user_config=json.loads(cmd_arg)
   print(cmd_arg)
-  print(type(cmd_arg)
+  print(type(cmd_arg))
   print(user_config)
-  print(type(user_config)
+  print(type(user_config))
